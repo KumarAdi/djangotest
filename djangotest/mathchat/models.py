99e4d4eaf1142452912883e929b95eaf3e33fbee
Aditya Kumar <adikumar@coconut.aca.uw.edu>
2018-01-08 11:42:34 -0800

    added Chatroom and Message models to db, created admin site

--- djangotest/mathchat/models.py
+++ djangotest/mathchat/models.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class ChatRoom (models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__ (self):
+        return self.name
+
+class Message (models.Model):
+    body = models.CharField(max_length = 200);
+    room = models.ForeignKey(ChatRoom, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.body
