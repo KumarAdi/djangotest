cd29a93cd72492f5a4ce0d2c4e534fb3dd918239
Aditya Kumar <adikumar@coconut.aca.uw.edu>
2018-01-11 09:27:16 -0800

    added mathquill for math rendering

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'mathchat.apps.MathchatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'asgi_redis.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('localhost', 6379)]
+        },
+        'ROUTING': 'mathchat.routing.channel_routing',
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -119,3 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- djangotest/mathchat/views.py
+++ djangotest/mathchat/views.py
@@ -2,10 +2,22 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import ChatRoom, Message
 
 def index (request):
     return HttpResponse("Hello, World - This is the mathchat index.")
+
+def room (request, room_name):
+    template = loader.get_template('mathchat/room.html')
+    messages = ChatRoom.objects.get(name=room_name).message_set.all()
+    context = {
+        'room_title': room_name,
+        'message_list': messages
+    }
+
+    return HttpResponse(template.render(context, request))
